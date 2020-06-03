44571dc1832b2f7bc921127b60e86e08a631588a
piotrek1493 <piotrek1493@gazeta.pl>
2020-06-03 04:03:08 +0200

    Refactoring code.

--- src/LeetcodeSolutions/Easy/ReverseWordsInAStringIII.java
+++ src/LeetcodeSolutions/Easy/ReverseWordsInAStringIII.java
@@ -22,8 +22,7 @@ public class ReverseWordsInAStringIII {
         char[] s = orig.toCharArray();
         int n = s.length;
         int halfLength = n / 2;
-        for (int i = 0; i < halfLength; i++)
-        {
+        for (int i = 0; i < halfLength; i++) {
             char temp = s[i];
             s[i] = s[n-1-i];
             s[n-1-i] = temp;
